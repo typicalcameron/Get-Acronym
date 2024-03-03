a7317760773d35c073547e2309a2615ace9d989e
typicalcameron <53804604+typicalcameron@users.noreply.github.com>
2024-03-02 23:34:47 -0500

    Initial Commit

---
+++ Get-Acronym.ps1
@@ -0,0 +1,23 @@
+﻿function Get-Acronym
+{
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$message
+    )
+    
+    # \s regex matches a whitespace character.
+    # .ToUpper() captilizes all the words
+    $split = ($message -split "\s").ToUpper()
+    
+    # Final output array.
+    $output = @()
+
+    # Foreach all splits and grabs the first character of each word/string using [0].
+    foreach ($word in $split)
+    {
+        $output += $word[0]
+    }
+    
+    # Each item in the array is joined together in a single string.
+    $output -join ""
+}
